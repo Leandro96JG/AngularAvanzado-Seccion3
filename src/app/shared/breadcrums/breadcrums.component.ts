8b0da1b7822e113ce28881edc702ce152d02137c
Leandro J. Gonzalez <Jesuusgonzalees@gmail.com>
2024-06-02 21:00:29 -0300

    Incio de angular avanzado

---
+++ src/app/shared/breadcrums/breadcrums.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'shared-breadcrums',
+  templateUrl: './breadcrums.component.html',
+  styleUrl: './breadcrums.component.css'
+})
+export class BreadcrumsComponent {
+
+}
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+
+}
