850b20ebd53fcf749583bdbf4df9122bcaafe41c
Leandro J. Gonzalez <Jesuusgonzalees@gmail.com>
2024-06-25 17:45:57 -0300

    rutas hijas - Seccion 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesRoutingModule } from './pages/pages-routing.module';
+
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+import { AuthRoutingModule } from './auth/auth-routing.module';
 
 const routes: Routes = [
   {
-    path:'',
-    component:PagesComponent,
-    children:[
-      {path:'dashboard', component:DashboardComponent,},
-      {path:'progress', component:ProgressComponent,},
-      {path:'grafica1', component:Grafica1Component,},
-      {path:'', redirectTo:'/dashboard',  pathMatch:'full',},
-    ],
+    path:'auth',
+    loadChildren: ()=>import('./auth/auth.module').then(m=>m.AuthModule),
   },
-
-  {path:'login', component:LoginComponent,},
-  {path:'register', component:RegisterComponent,},
-
-  {path:'**', component:NopagefoundComponent,},
+  {
+    path:'dashboard',
+    loadChildren: ()=>import('./pages/pages.module').then(m=>m.PagesModule),
+    pathMatch:'full'
+  },
+  {
+    path:'',
+    redirectTo:'/auth',
+    pathMatch:'full'
+  },
+  {path:'**', component:NopagefoundComponent},
 ];
 
 @NgModule({
   //forRoot es para rutas principales
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+            PagesRoutingModule,
+            AuthRoutingModule,
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
